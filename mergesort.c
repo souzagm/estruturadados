9130b65fb80aef0dc34b8e32404419b5fca7cfaa
Guilherme Souza <guilhermemelo17@gmail.com>
2022-08-23 11:18:32 -0400

    primeiro commit

---
+++ mergesort.c
@@ -0,0 +1,131 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+
+#define TAM 10
+#define MIN 1
+#define MAX 100
+
+/* C program for Merge Sort */
+// https://pt.stackoverflow.com/questions/136206/gerar-números-randômicos-que-não-se-repetem
+#include <stdio.h>
+ int main()
+{
+    int arr[TAM];
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+ 
+    // alimentando vetor 
+    feedArray(arr);
+
+    printf("Given array is \n");
+    printArray(arr, arr_size);
+ 
+    mergeSort(arr, 0, arr_size - 1);
+ 
+    printf("\nSorted array is \n");
+    printArray(arr, arr_size);
+    return 0;
+}
+// Merges two subarrays of arr[].
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+ 
+    /* create temp arrays */
+    int L[n1], R[n2];
+ 
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+ 
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    /* Copy the remaining elements of L[], if there
+    are any */
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+    /* Copy the remaining elements of R[], if there
+    are any */
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+ 
+/* l is for left index and r is right index of the
+sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r) {
+        // Same as (l+r)/2, but avoids overflow for
+        // large l and h
+        int m = l + (r - l) / 2;
+ 
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+ 
+        merge(arr, l, m, r);
+    }
+}
+ 
+/* UTILITY FUNCTIONS */
+/* Function to print an array */
+void printArray(int A[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", A[i]);
+    printf("\n");
+}
+
+
+void feedArray(int arr[])
+{
+   srand(time(NULL));
+    int * numeros = malloc((MAX - MIN) * sizeof(int));
+    if (!numeros) exit(EXIT_FAILURE);
+    for (int i = 0; i < MAX - MIN; i++) {
+        numeros[i] = i + MIN;
+    }
+    shuffle(numeros);
+    for (int i = 0; i < TAM; i++) {
+       arr[i]= numeros[i];
+    }
+     
+}
+
+void shuffle(int *array) {
+    for (int i = MAX - MIN - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        int tmp = array[j];
+        array[j] = array[i];
+        array[i] = tmp;
+    }
+}
